347f8acaf3e756ef52dba419ca7fd78c8b1bfaa8
Alex Korenkova <alex.korenkova@gmail.com>
2023-06-09 13:06:44 +0300

    basePath added

--- next.config.js
+++ next.config.js
@@ -1,43 +1,44 @@
 module.exports = {
-	images: {
-		domains: ['courses-top.ru']
-	},
-	reactStrictMode: true,
-	webpack(config) {
-		config.module.rules.push({
-			test: /\.svg?$/,
-			oneOf: [
-				{
-					use: [
-						{
-							loader: '@svgr/webpack',
-							options: {
-								prettier: false,
-								svgo: true,
-								svgoConfig: {
-									plugins: [
-										{
-											name: 'preset-default',
-											params: {
-												overrides: {
-													// disable plugins
-													removeViewBox: false,
-												},
-											},
-										},
-									],
-								},
-								titleProp: true,
-							},
-						},
-					],
-					issuer: {
-						and: [/\.(ts|tsx|js|jsx|md|mdx)$/],
-					},
-				},
-			],
-		});
+    images: {
+        domains: ['courses-top.ru'],
+    },
+    basePath: '/top-app',
+    reactStrictMode: true,
+    webpack(config) {
+        config.module.rules.push({
+            test: /\.svg?$/,
+            oneOf: [
+                {
+                    use: [
+                        {
+                            loader: '@svgr/webpack',
+                            options: {
+                                prettier: false,
+                                svgo: true,
+                                svgoConfig: {
+                                    plugins: [
+                                        {
+                                            name: 'preset-default',
+                                            params: {
+                                                overrides: {
+                                                    // disable plugins
+                                                    removeViewBox: false,
+                                                },
+                                            },
+                                        },
+                                    ],
+                                },
+                                titleProp: true,
+                            },
+                        },
+                    ],
+                    issuer: {
+                        and: [/\.(ts|tsx|js|jsx|md|mdx)$/],
+                    },
+                },
+            ],
+        });
 
-		return config;
-	},
+        return config;
+    },
 };
